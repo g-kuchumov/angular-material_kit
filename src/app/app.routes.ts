0c63cf37544ac85a2032e5a2c738fa4c7dda3f59
Георгий Кучумов <g.kuchumov@bingosoft.ru>
2026-08-04 16:29:53 +0300

    feat: добавлена страница с элементами типа checkbox

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Card } from './shared/components/card/card';
 import { Button } from './shared/components/button/button';
 import { ButtonToggle } from './shared/components/button-toggle/button-toggle';
+import { Checkbox } from './shared/components/checkbox/checkbox';
 
 export const routes: Routes = [
   { path: 'button-toggle', component: ButtonToggle },
   { path: 'button', component: Button },
   { path: 'card', component: Card },
+  { path: 'checkbox', component: Checkbox },
 ];
---
+++ src/app/shared/components/checkbox/checkbox.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatCheckbox } from '@angular/material/checkbox';
+
+@Component({
+  selector: 'amk-checkbox',
+  imports: [MatCheckbox],
+  templateUrl: './checkbox.html',
+  styleUrl: './checkbox.scss',
+})
+export class Checkbox {}
